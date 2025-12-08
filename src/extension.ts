88ae4840ee015f802d91fa6dfcbc987df2734e1a
Sebastian Safari <ssebs@ymail.com>
2025-12-08 13:33:06 -0800

    initial

---
+++ src/extension.ts
@@ -0,0 +1,20 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	console.log('Congratulations, your extension "mdplan" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('mdplan.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from mdplan!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
---
+++ test_folder/Project1.md
@@ -0,0 +1,31 @@
+# Project 1
+
+> Smol description of the project.
+
+## MVP
+- [wip] Create todo list / README
+- [ ] Init repo
+  - [ ] Create folder
+  - [ ] git init
+  - [ ] Create git repo
+- [ ] Create basic UI
+  - [ ] Create index.html
+  - [ ] Link `styles.css`
+    - There should be dark mode support
+- [blocked] Get approval
+  - @ssebs
+  - ```md
+    - Note: approval is needed from the CEO before we can go-live.
+    The CEO is the big kahuna, so he get's final say.
+    ![img](./broken-link)
+    ```
+  - > blocked bc we need approval from the CEO first. @2025-12-08 @ssebs
+  - > approved - @2025-12-19 - @ceo
+
+## Next phase
+> Short desc
+- [ ] Add a button to the UI
+- [ ] Make the button do something
+
+## [Done]
+- Think about the project
